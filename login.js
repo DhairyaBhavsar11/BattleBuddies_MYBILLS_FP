73bc147b9574a1f1fa856189cdab3edd3f8b412e
Jinesh <105523801+jineshlambton@users.noreply.github.com>
2022-08-14 15:51:19 -0400

    Store signup data to local storage

--- login.js
+++ login.js
@@ -1,3 +1,17 @@
+function store(){
+    var inputFname = document.getElementById("Fname");
+    var inputLname = document.getElementById("Lname");
+    var inputEmail = document.getElementById("email");
+    var inputPassword = document.getElementById("Spassword");
+    
+    localStorage.setItem("Fname", inputFname.value);
+    localStorage.setItem("Lname", inputLname.value);
+    localStorage.setItem("email", inputEmail.value);
+    localStorage.setItem("Spassword", inputPassword.value);
+    
+    }
+    
+
 function validate(){
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
